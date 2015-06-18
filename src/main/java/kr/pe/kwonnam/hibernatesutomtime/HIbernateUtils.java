0f48af709b6b09e8371587e88ff95e4b81f2903e
KwonNam Son <kwon37xi@gmail.com>
2015-06-18 20:15:19 +0900

    initialize project

---
+++ src/main/java/kr/pe/kwonnam/hibernatesutomtime/HIbernateUtils.java
@@ -0,0 +1,25 @@
+package kr.pe.kwonnam.hibernatesutomtime;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HIbernateUtils {
+    private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+
+    public static  void shutdown() {
+        getSessionFactory().close();
+    }
+}
---
+++ src/main/java/kr/pe/kwonnam/hibernatesutomtime/User.java
@@ -0,0 +1,71 @@
+package kr.pe.kwonnam.hibernatesutomtime;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ *
+ */
+@Entity
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 20)
+    private String name;
+
+    @Type(type = "true_false")
+    @Column(name = "employee", columnDefinition = "varcher(5)")
+    private Boolean employee;
+
+    @Type(type = "yes_no")
+    @Column(name = "male", columnDefinition = "varchar(5)")
+    private Boolean male;
+
+    @Column(name = "old")
+    private Boolean old;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Boolean employee) {
+        this.employee = employee;
+    }
+
+    public Boolean getMale() {
+        return male;
+    }
+
+    public void setMale(Boolean male) {
+        this.male = male;
+    }
+
+    public Boolean getOld() {
+        return old;
+    }
+
+    public void setOld(Boolean old) {
+        this.old = old;
+    }
+}
